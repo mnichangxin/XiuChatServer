519f3a6eea4357fdaf3ab5f300846c3ff5d234b9
mnichangxin <mnichangxin@163.com>
2018-03-21 20:36:55 +0800

    commit

--- routes/register.js
+++ routes/register.js
@@ -4,10 +4,24 @@ var User = require('../models/user')
 module.exports = function(req, res) {
     var data = req.body
     
-    User.create({
-        name: data.name,
-        password: data.password
-    }, function(err) {
-        res.send('Register success!')
+    User.find({name: data.name}, function(err) {
+        if (err) {
+            res.json({
+                status: 0,
+                msg: 'fail'
+            })
+        } else {
+            res.json({
+                status: 1,
+                msg: 'success'
+            })
+        }
     })
+    
+    // User.create({
+    //     name: data.name,
+    //     password: data.password
+    // }, function(err) {
+    //     res.send('Register success!')
+    // })
 }
