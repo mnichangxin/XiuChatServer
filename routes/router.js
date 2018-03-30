d8c52298b9af719a1ad3da0dddf9a8c08ed39cb2
mnichangxin <mnichangxin@163.com>
2018-03-30 18:31:49 +0800

    'commit'

--- routes/router.js
+++ routes/router.js
@@ -7,10 +7,12 @@ var login = require('./login')
 var register = require('./register')
 var follow = require('./follow')
 var unfollow = require('./unfollow')
+var findFollowing = require('./findFollowing')
 
 router.post('/login', login)
 router.post('/register', register)
 router.post('/follow', follow)
 router.post('/unfollow', unfollow)
+router.get('/findFollowing', findFollowing)
 
 module.exports = router
