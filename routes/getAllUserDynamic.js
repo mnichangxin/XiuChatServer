3b51b29b3d122f9c87e2fbd1ccab3e087f5cecfb
mnichangxin <mnichangxin@163.com>
2018-05-04 03:04:51 +0800

    add api

--- routes/getAllUserDynamic.js
+++ routes/getAllUserDynamic.js
@@ -1,5 +1,6 @@
 /* 获取全部用户动态路由 */
 var Dynamic = require('../models/dynamic')
+var UserInfo = require('../models/userInfo')
 
 module.exports = function(req, res) {
     var data = req.query
@@ -16,11 +17,39 @@ module.exports = function(req, res) {
                 res.send(err)
                 return
             } else {
-                res.send({
-                    status: 1,
-                    msg: '获取成功',
-                    data: docs
+                var newDocs = []
+
+                docs.forEach(function(doc) {
+                    UserInfo.findOne({
+                        _id: doc.origin_id
+                    }, function(err, docc) {
+                        if (err) {
+                            res.send(err)
+                            return
+                        } else {
+                            var tDoc = {
+                                _id: doc._id,
+                                origin_id: doc.origin_id,
+                                nickname: docc.nickname,
+                                share: doc.share,
+                                type: doc.type,
+                                story: doc.story,
+                                fav: doc.fav,
+                                commit: doc.commit
+                            }
+
+                            newDocs.push(tDoc)
+                        }
+                    })
                 })
+
+                setTimeout(() => {
+                    res.send({
+                        status: 1,
+                        msg: '获取成功',
+                        data: newDocs
+                    })
+                }, 50)
             }
         })
 }
